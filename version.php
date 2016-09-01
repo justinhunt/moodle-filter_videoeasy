89ea0c5c12f56872c17ab3b044151e78df8022be
Justin Hunt <poodllsupport@gmail.com>
2016-08-31 21:26:14 -0400

    more preset fixes for install logic

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016082003;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2016082004;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2015051100;        // Requires this Moodle version
 $plugin->component = 'filter_videoeasy'; // Full name of the plugin (used for diagnostics)
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.1.7(Build 2016082003)';
+$plugin->release   = '1.1.7(Build 2016082004)';
 
